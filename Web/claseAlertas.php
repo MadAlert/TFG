995c7f57ca8a63db519178d283304e03498d7317
AdriPanG <adripang92@gmail.com>
2018-02-19 21:29:14 +0100

    Mostrado el numero de alertas de cada distrito en el mapa

--- Web/claseAlertas.php
+++ Web/claseAlertas.php
@@ -52,7 +52,6 @@ class claseAlertas {
     public function obtenerAlertasDistrito($distrito) {
    		$coleccion = $this->conexion();
    		
-    	
     	$documento = $coleccion->find(['distrito' => $distrito]);
     	$total = $coleccion->count(['distrito' => $distrito]);
     	if($total > 0){
@@ -85,6 +84,64 @@ class claseAlertas {
 	    }
 
     	
+    }
+
+    //Obtiene el numero de alertas de todos los distristos
+    public function obtenerNumeroAlertas($distrito) {
+   	  $coleccion = $this->conexion();
+
+      $total = $coleccion->count(['distrito' => $distrito]);
+      return $total;	
+    }
+
+    public function crearCamposOcultosNumAlertas($lista){
+      echo ' <input type="hidden" name="arganzuela" value='.$lista[0].' id="arganzuela"/>';
+      echo ' <input type="hidden" name="barajas" value='.$lista[1].' id="barajas"/>';
+      echo ' <input type="hidden" name="carabanchel" value='.$lista[2].' id="carabanchel"/>';
+      echo ' <input type="hidden" name="centro" value='.$lista[3].' id="centro"/>';
+      echo ' <input type="hidden" name="chamartin" value='.$lista[4].' id="chamartin"/>';
+      echo ' <input type="hidden" name="chamberi" value='.$lista[5].' id="chamberi"/>';
+      echo ' <input type="hidden" name="ciudadlineal" value='.$lista[6].' id="ciudadlineal"/>';
+      echo ' <input type="hidden" name="fuencarral" value='.$lista[7].' id="fuencarral"/>';
+      echo ' <input type="hidden" name="hortaleza" value='.$lista[8].' id="hortaleza"/>';
+      echo ' <input type="hidden" name="latina" value='.$lista[9].' id="latina"/>';
+      echo ' <input type="hidden" name="moncloa" value='.$lista[10].' id="moncloa"/>';
+      echo ' <input type="hidden" name="moratalaz" value='.$lista[11].' id="moratalaz"/>';
+      echo ' <input type="hidden" name="puentevallecas" value='.$lista[12].' id="puentevallecas"/>';
+      echo ' <input type="hidden" name="retiro" value='.$lista[13].' id="retiro"/>';
+      echo ' <input type="hidden" name="salamanca" value='.$lista[14].' id="salamanca"/>';
+      echo ' <input type="hidden" name="sanblas" value='.$lista[15].' id="sanblas"/>';
+      echo ' <input type="hidden" name="tetuan" value='.$lista[16].' id="tetuan"/>';
+      echo ' <input type="hidden" name="usera" value='.$lista[17].' id="usera"/>';
+      echo ' <input type="hidden" name="vicalvaro" value='.$lista[18].' id="vicalvaro"/>';
+      echo ' <input type="hidden" name="villavallecas" value='.$lista[19].' id="villavallecas"/>';
+      echo ' <input type="hidden" name="villaverde" value='.$lista[20].' id="villaverde"/>';
+    }
+
+    public function obtenerDatos (){
+      $totalArganzuela= $this->obtenerNumeroAlertas("Arganzuela");
+      $totalBarajas = $this->obtenerNumeroAlertas("Barajas");
+      $totalCarabanchel = $this->obtenerNumeroAlertas("Carabanchel");
+      $totalCentro = $this->obtenerNumeroAlertas("Centro");
+      $totalChamartin = $this->obtenerNumeroAlertas("Chamartín");
+      $totalChamberi = $this->obtenerNumeroAlertas("Chamberí");
+      $totalCiudadLineal = $this->obtenerNumeroAlertas("Ciudad Lineal");
+      $totalFuencarral = $this->obtenerNumeroAlertas("Fuencarral-ElPardo");
+      $totalHortaleza = $this->obtenerNumeroAlertas("Hortaleza");
+      $totalLatina = $this->obtenerNumeroAlertas("Latina");
+      $totalMoncloa = $this->obtenerNumeroAlertas("Moncloa-Aravaca");
+      $totalMoratalaz = $this->obtenerNumeroAlertas("Moratalaz");
+      $totalPuenteVallecas = $this->obtenerNumeroAlertas("Puente de Vallecas");
+      $totalRetiro = $this->obtenerNumeroAlertas("Retiro");
+      $totalSalamanca = $this->obtenerNumeroAlertas("Salamanca");
+      $totalSanBlas = $this->obtenerNumeroAlertas("San Blas-Canillejas");
+      $totalTetuan = $this->obtenerNumeroAlertas("Tetuán");
+      $totalUsera = $this->obtenerNumeroAlertas("Usera");
+      $totalVicalvaro = $this->obtenerNumeroAlertas("Vicálvaro");
+      $totalVillaVallecas = $this->obtenerNumeroAlertas("Villa de Vallecas");
+      $totalVillaverde = $this->obtenerNumeroAlertas("Villaverde");
+      $lista = [$totalArganzuela, $totalBarajas, $totalCarabanchel, $totalCentro, $totalChamartin, $totalChamberi, $totalCiudadLineal, $totalFuencarral, $totalHortaleza, $totalLatina, $totalMoncloa, $totalMoratalaz, $totalPuenteVallecas, $totalRetiro, $totalSalamanca, $totalSanBlas, $totalTetuan, $totalUsera, $totalVicalvaro, $totalVillaVallecas, $totalVillaverde];
+      return $lista;
     }
 
 
---
+++ Web/js/javaScriptMapa.js
@@ -0,0 +1,75 @@
+function initMap() {
+    var marcadores = [
+        ['Arganzuela', 40.400861, -3.699350, document.getElementById("arganzuela").value], //Arganzuela
+        ['Barajas', 40.4839402, -3.5701402, document.getElementById("barajas").value], //Barajas
+        ['Carabanchel', 40.381607, -3.735203, document.getElementById("carabanchel").value], //Carabanchel
+        ['Centro', 40.4169416, -3.7083759, document.getElementById("centro").value], //Centro
+        ['Chamartín', 40.460367, -3.676567, document.getElementById("chamartin").value], //Chamartín
+        ['Chamberí', 40.438656, -3.704180, document.getElementById("chamberi").value], //Chamberí
+        ['Ciudad Lineal', 40.455531, -3.656119, document.getElementById("ciudadlineal").value], //Ciudad Lineal
+        ['Fuencarral-ElPardo', 40.494289, -3.693477, document.getElementById("fuencarral").value], //Fuencarral-ElPardo
+        ['Hortaleza', 40.485152, -3.634796, document.getElementById("hortaleza").value], //Hortaleza
+        ['Latina', 40.387812, -3.773530, document.getElementById("latina").value], //Latina
+        ['Moncloa-Aravaca', 40.443568,  -3.742829, document.getElementById("moncloa").value], //Moncloa-Aravaca
+        ['Moratalaz', 40.407016, -3.644330, document.getElementById("moratalaz").value], //Moratalaz
+        ['Puente de Vallecas', 40.386887, -3.658476, document.getElementById("puentevallecas").value], //Puente de Vallecas
+        ['Retiro', 40.4101076, -3.6736514, document.getElementById("retiro").value], //Retiro
+        ['Salamanca', 40.429807, -3.673778, document.getElementById("salamanca").value], //Salamanca
+        ['San Blas-Canillejas', 40.436229, -3.599431, document.getElementById("sanblas").value], //San Blas-Canillejas
+        ['Tetuán', 40.460158, -3.698835, document.getElementById("tetuan").value], //Tetuán
+        ['Usera', 40.377026, -3.701982, document.getElementById("usera").value], //Usera
+        ['Vicálvaro', 40.393974, -3.581134, document.getElementById("vicalvaro").value], //Vicálvaro
+        ['Villa de Vallecas', 40.355089, -3.621192, document.getElementById("villavallecas").value], //Villa de Vallecas
+        ['Villaverde', 40.345987, -3.693332, document.getElementById("villaverde").value] //Villaverde
+    ];
+
+    var map = new google.maps.Map(document.getElementById('mapa'),{
+        zoom: 12,
+        zoomControl: false,
+        streetViewControl: false,
+        scrollwheel: false,
+        center: new google.maps.LatLng(40.422163, -3.689101),
+    });
+
+    var infowindow = new google.maps.InfoWindow(); //Abre ventana del marcador
+        // Create an array of alphabetical characters used to label the markers.
+        //var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        /*var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: locations,
+            label: labels[i % labels.length]
+          });
+        });*/
+
+    var marker, i;
+    var cuentaAlertas = [];
+    for (i = 0; i < marcadores.length; i++) {
+        if(marcadores[i][3] != 0) {  //Solo muestra los distritos que tienen alertas
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(marcadores[i][1], marcadores[i][2]),
+                map: map
+            });
+        }
+        
+
+            
+              //'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+              //'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+              //'(last visited June 22, 2009).</p>';
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                if(marcadores[i][3] != 0) {
+                    var contentString = '<h1>' + marcadores[i][0] +'</h1>'+
+                    '<p> Se han encontrado <b>' + marcadores[i][3] + ' alertas</b>. </p>';
+                    infowindow.setContent(contentString);
+                    infowindow.open(map, marker);
+                }
+            }
+        })(marker, i));
+    }
+
+}
+      
+google.maps.event.addDomListener(window, 'load', initMap);
