5e85ecbfa5e356bdcd062f65426142e52bc6c600
Gonzalo Molina <gonzalo14molina@gmail.com>
2018-03-22 21:32:16 +0100

    Android
    Subiendo un proyecto nuevo que va bien

---
+++ Android/MadAlert/app/src/main/java/a/madalert/madalert/tabpagerAdapter.java
@@ -0,0 +1,47 @@
+package a.madalert.madalert;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by gmolindi on 21/03/2018.
+ */
+
+public class tabpagerAdapter extends FragmentStatePagerAdapter {
+
+    private String[] tabArray = new String[]{"Alertas", "Mapa", "Distritos"};
+    private Integer tabNumber = tabArray.length;
+
+    public tabpagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return tabArray[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                alertas aler = new alertas();
+                return aler;
+            case 1:
+                mapa map = new mapa();
+                return map;
+            case 2:
+                distritos dist = new distritos();
+                return dist;
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabNumber;
+    }
+}
