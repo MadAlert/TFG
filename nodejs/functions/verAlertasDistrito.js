01df027be43fc2741ce17edbd76ef8024fd4d941
SilviaLenFer <sillendi@ucm.es>
2018-04-19 19:51:36 +0200

    Revert "Merge branch 'master' of https://github.com/MadAlert/TFG"
    This reverts commit 974b53454a912b5fec58ef20f5bb99fe92dcfd53, reversing
    changes made to 8e1fefe4e68ed566ef23447fbba647a40f7dd936.

--- nodejs/functions/verAlertasDistrito.js
+++ nodejs/functions/verAlertasDistrito.js
@@ -6,7 +6,7 @@ exports.getAlertasDistrito = distrito =>
 
 	new Promise((resolve, reject) => {
 		
-		alerta.find({distrito: distrito}).sort({fecha:-1})
+		alerta.find({distrito: distrito})
 
 		.then(alertas => resolve(alertas))
 			
