c3b1cc5f5b63a89e612e376647d35bf55b4ce138
gonzamol <gonzalo14molina@gmail.com>
2018-04-21 12:12:16 +0200

    switch y distritos
    Queda guardado si el switch esta activado/desactivado y el distrito seleccionado.

--- Android/MadAlert/app/src/main/java/a/madalert/madalert/ConfigActivity.java
+++ Android/MadAlert/app/src/main/java/a/madalert/madalert/ConfigActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.GridView;
 import android.widget.SeekBar;
@@ -16,9 +17,6 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -31,28 +29,68 @@ public class ConfigActivity extends AppCompatActivity implements CompoundButton.
             "Todas", "Desastres y accidentes", "Terrorismo", "Criminalidad",
             "Tráfico", "Eventos", "Transporte público", "Contaminación"};
 
-    private GridView gridView;
     private SharedPreferences mSharedPreferences;
-    private CompositeDisposable mSubscriptions;
 
-    private SeekBar seekBar;
     private TextView muestrKm;
-    private Switch swUbi;
     private Spinner spinner;
     private int km;
+    private SharedPreferences.Editor editor;
+    private boolean isCheckedSw;
+    private String distrito; // ahora mismo no está siendo usado pero lo será en un futuro
+    private int pos;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
-        mSubscriptions = new CompositeDisposable();
+        CompositeDisposable mSubscriptions = new CompositeDisposable();
         initSharedPreferences();
-        SharedPreferences.Editor editor = mSharedPreferences.edit();
-        seekBar = (SeekBar) findViewById(R.id.seekBarRadio);
+        editor = mSharedPreferences.edit(); // para guardar las configuraciones
+
+        initSeekBar();
+        initGridView();
+        initSwitch();
+
+    }
+
+    private void initSharedPreferences() {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        km = mSharedPreferences.getInt("km", -1);
+        isCheckedSw = mSharedPreferences.getBoolean("isCheckedSw", true);
+       // distrito = mSharedPreferences.getString("distrito", "");
+        pos = mSharedPreferences.getInt("posicion", -1);
+        // falta guardar las categorias, recuerda lo pasos: put, get y set. QUE NO SE TE OLVIDE EL SET GONZA COÑO
+    }
+
+    private void initSwitch(){
+        Switch swUbi = (Switch) findViewById(R.id.switchUbi);
+        swUbi.setChecked(isCheckedSw);
+
+        if(swUbi != null)
+            swUbi.setOnCheckedChangeListener(this);
+
+        initSpinner();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Toast.makeText(this, "La ubicación está " + (isChecked ? "activada" : "desactivada"),
+                Toast.LENGTH_SHORT).show();
+
+        isCheckedSw = isChecked;
+        editor.putBoolean("isCheckedSw", isCheckedSw);
+        Log.d("BOOL", String.valueOf(isChecked));
+        editor.apply();
+
+        initSpinner();
+    }
+
+    private void initSeekBar(){
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBarRadio);
         seekBar.setProgress(km);
 
-       // obtiene el valor de los km
+        // obtiene el valor de los km
         muestrKm = findViewById(R.id.tvKm);
 
         muestrKm.setText(km + " km");
@@ -76,8 +114,10 @@ public class ConfigActivity extends AppCompatActivity implements CompoundButton.
 
             }
         });
+    }
 
-        gridView = (GridView) findViewById(R.id.grid);
+    private void initGridView(){
+        GridView gridView = (GridView) findViewById(R.id.grid);
 
         selectedStrings = new ArrayList<>();
 
@@ -95,34 +135,36 @@ public class ConfigActivity extends AppCompatActivity implements CompoundButton.
                 selectedStrings.add((String) parent.getItemAtPosition(position));
             }
         });
-
-        swUbi = (Switch) findViewById(R.id.switchUbi);
-
-        if(swUbi != null)
-            swUbi.setOnCheckedChangeListener(this);
-
-        // Guarda los cambios realizados
-
-        editor.putString("swUbi", swUbi.toString());
-        editor.putString("distrito",  "lo que queda");
-        editor.apply();
-
     }
 
-    private void initSharedPreferences() {
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        km = mSharedPreferences.getInt("km", 0);
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        Toast.makeText(this, "La ubicación está " + (isChecked ? "activada" : "desactivada"),
-                Toast.LENGTH_SHORT).show();
+    private void initSpinner(){
         spinner = (Spinner) findViewById(R.id.spinnerDistritos);
-        if(isChecked) {
+        spinner.setSelection(pos);
+
+        if(isCheckedSw) {
             spinner.setVisibility(View.INVISIBLE);
         } else {
             spinner.setVisibility(View.VISIBLE);
         }
+
+        // Esto sirve para que se quede guardado el distrito seleccionado
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                //distrito = spinner.toString(); ESTO HABRÁ QUE GUARDARLO COMO STRING TB CUANDO HAGA LA CONSULTA..
+                pos = spinner.getSelectedItemPosition();
+                Log.d("TAG POS", String.valueOf(pos));
+                //editor.putString("distrito",  distrito);
+                editor.putInt("posicion", pos);
+                editor.apply();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+
+            }
+        });
+
+
+
     }
 }
