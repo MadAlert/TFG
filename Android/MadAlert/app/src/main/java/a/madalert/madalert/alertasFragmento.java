dbc25a4acfa253b8da58777a210176b18864fa8b
AdriPanG <adripang92@gmail.com>
2018-04-26 12:30:15 +0200

    Añadida búsqueda por categorias

--- Android/MadAlert/app/src/main/java/a/madalert/madalert/alertasFragmento.java
+++ Android/MadAlert/app/src/main/java/a/madalert/madalert/alertasFragmento.java
@@ -28,8 +28,6 @@ public class AlertasFragmento extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_alertas, container, false);
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.anadirAlerta);
