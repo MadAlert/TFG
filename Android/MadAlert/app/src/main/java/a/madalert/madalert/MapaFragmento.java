c4d2e2778c90afe1beb98d7ee853d5e420edc2b0
AdriPanG <adripang92@gmail.com>
2018-05-13 22:50:32 +0200

    Marcadores del mapa añadidos

--- Android/MadAlert/app/src/main/java/a/madalert/madalert/MapaFragmento.java
+++ Android/MadAlert/app/src/main/java/a/madalert/madalert/MapaFragmento.java
@@ -2,34 +2,56 @@ package a.madalert.madalert;
 
 
 import android.Manifest;
-import android.accessibilityservice.AccessibilityService;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapaFragmento extends Fragment implements LocationListener {
+public class MapaFragmento extends Fragment implements
+        GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback {
 
-    private static final String TAG = MapaFragmento.class.getSimpleName();
-    private LocationManager mLocationManager;
-    private TextView tvLat, tvLon;
-    private Button btnGo, btnStop;
-
+    private MapView mapView;
+    private GoogleMap map;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor editor;
+    private String latitud;
+    private String longitud;
+    private int km;
+    private HashMap<String, Pair<Double, Double>> distCoord;
 
     public MapaFragmento() {
         // Required empty public constructor
@@ -42,80 +64,113 @@ public class MapaFragmento extends Fragment implements LocationListener {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_mapa, container, false);
 
-        mLocationManager = (LocationManager) this.getContext().getSystemService(Context.LOCATION_SERVICE);
-       /* tvLat = (TextView) v.findViewById(R.id.textViewLatitud);
-        tvLon = (TextView) v.findViewById(R.id.textViewLongitud);
-        btnGo = v.findViewById(R.id.button1);
-        btnStop = v.findViewById(R.id.button2);*/
+        initSharedPreferences();
+        initCoord();
 
-        /*btnGo.setOnClickListener(view -> {
-
-            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                Log.d(TAG,"Faltan permisos");
-                return;
-            }
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
-        });
-
-        btnStop.setOnClickListener(view -> {
-            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                Log.d(TAG,"Faltan permisos");
-                return;
-            }
-            mLocationManager.removeUpdates(this);
-        });
-*/
         return v;
     }
 
-    public void onClickGo(View view) {
-        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    private void initSharedPreferences() {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        latitud = mSharedPreferences.getString("latitud", "");
+        longitud = mSharedPreferences.getString("longitud", "");
+        km = mSharedPreferences.getInt("km", 0);
+    }
 
-            Log.d(TAG,"Faltan permisos");
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) view.findViewById(R.id.mapG);
+
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+        map = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
-    }
+        map.setMyLocationEnabled(true);
 
-    public void onClickStop(View view) {
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        double latitud2 = Double.parseDouble(latitud);
+        double longitud2 = Double.parseDouble(longitud);
 
-            Log.d(TAG,"Faltan permisos");
-            return;
+
+
+        int kilometros = km * 1000;
+
+        Log.d("kilometros", String.valueOf(kilometros));
+
+        Circle circle = map.addCircle(new CircleOptions()
+                .center(new LatLng(latitud2, longitud2))
+                .radius(kilometros)
+                .strokeColor(Color.BLUE)
+                .fillColor(0x220000FF)
+                .strokeWidth(5));
+
+
+        Iterator<Map.Entry<String, Pair<Double, Double>>> iterator = distCoord.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<String, Pair<Double, Double>> it = iterator.next();
+            Double lat = it.getValue().first;
+            Double longi = it.getValue().second;
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, longi)).title(it.getKey()).snippet("Se han encontrado 0 alertas")).setTag(0);
         }
-        mLocationManager.removeUpdates(this);
+
+        // Set a listener for marker click.
+        map.setOnInfoWindowClickListener(this);
+
+        CameraPosition camera = CameraPosition.builder().target(circle.getCenter()/*new LatLng(latitud2, longitud2)*/).zoom(12).bearing(0).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(camera));
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG,"Faltan permisos");
-            return;
-        }
-        mLocationManager.removeUpdates(this);
+    public void initCoord() {
+        distCoord = new HashMap<String, Pair<Double, Double>>();
+        distCoord.put("Arganzuela", new Pair<>(40.400861, -3.699350));
+        distCoord.put("Barajas", new Pair<>(40.4839402, -3.5701402));
+        distCoord.put("Carabanchel", new Pair<>(40.381607, -3.735203));
+        distCoord.put("Centro", new Pair<>(40.4169416, -3.7083759));
+        distCoord.put("Chamartín", new Pair<>(40.460367, -3.676567));
+        distCoord.put("Chamberí", new Pair<>(40.438656, -3.704180));
+        distCoord.put("Ciudad Lineal", new Pair<>(40.455531, -3.656119));
+        distCoord.put("Fuencarral-El Pardo", new Pair<>(40.494289, -3.693477));
+        distCoord.put("Hortaleza", new Pair<>(40.485152, -3.634796));
+        distCoord.put("Latina", new Pair<>(40.387812, -3.773530));
+        distCoord.put("Moncloa-Aravaca", new Pair<>(40.443568, -3.742829));
+        distCoord.put("Moratalaz", new Pair<>(40.407016, -3.644330));
+        distCoord.put("Puente de Vallecas", new Pair<>(40.386887, -3.658476));
+        distCoord.put("Retiro", new Pair<>(40.4101076, -3.6736514));
+        distCoord.put("Salamanca", new Pair<>(40.429807, -3.673778));
+        distCoord.put("San Blas-Canillejas", new Pair<>(40.436229, -3.599431));
+        distCoord.put("Tetuán", new Pair<>(40.460158, -3.698835));
+        distCoord.put("Usera", new Pair<>(40.377026, -3.701982));
+        distCoord.put("Vicálvaro", new Pair<>(40.393974, -3.581134));
+        distCoord.put("Villa de Vallecas", new Pair<>(40.355089, -3.621192));
+        distCoord.put("Villaverde", new Pair<>(40.345987, -3.693332));
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        tvLat.setText(location.getLatitude() + "");
-        tvLon.setText(location.getLongitude() + "");
-    }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(this.getActivity(), "Info window clicked",
+                Toast.LENGTH_LONG).show();
     }
 }
