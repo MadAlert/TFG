bb56d7058c09d8b1540468da39924834cba23830
AdriPanG <adripang92@gmail.com>
2018-05-15 11:36:18 +0200

    Corrección de la distancia en main activity

--- Android/MadAlert/app/src/main/java/a/madalert/madalert/MainActivity.java
+++ Android/MadAlert/app/src/main/java/a/madalert/madalert/MainActivity.java
@@ -191,21 +191,27 @@ public class MainActivity extends AppCompatActivity
     }
 
     private double distanciaCoordenadas(double latitudNueva, double longitudNueva){
-        double hipotenusa = 0;
+        double distancia = 0;
+        double radioTierra = 6371;//en kilómetros
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String strLatitudAntigua = mSharedPreferences.getString("latitud","");
         String strLongitudAntigua = mSharedPreferences.getString("longitud","");
+
         if(strLatitudAntigua!="" && strLongitudAntigua!=""){
             double latitudAntigua = Double.parseDouble(strLatitudAntigua);
             double longitudAntigua = Double.parseDouble(strLongitudAntigua);
-
-            double cateto1 = latitudAntigua -latitudNueva;
-            double cateto2 = longitudAntigua - longitudNueva;
-
-            hipotenusa = Math.sqrt((cateto1*cateto1)+(cateto2*cateto2));
+            double dLat = Math.toRadians(latitudAntigua - latitudNueva);
+            double dLng = Math.toRadians(longitudAntigua - longitudNueva);
+            double sindLat = Math.sin(dLat / 2);
+            double sindLng = Math.sin(dLng / 2);
+            double va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                    * Math.cos(Math.toRadians(latitudAntigua)) * Math.cos(Math.toRadians(latitudNueva));
+            double va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
+            distancia = radioTierra * va2;
         }
-        return hipotenusa;
+        return distancia;
     }
+
 
     // Aqui empieza la clase Localizacion
     public class Localizacion implements LocationListener {
