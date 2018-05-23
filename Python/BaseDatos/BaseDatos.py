2cc141b0aa54d9c11fde898cd49996c2dd15474c
gonzamol <gonzalo14molina@gmail.com>
2018-05-23 19:54:12 +0200

    limpiando codigo
    solo he quitado comentarios y algunos prints innecesarios

--- Python/BaseDatos/BaseDatos.py
+++ Python/BaseDatos/BaseDatos.py
@@ -4,46 +4,32 @@ from pymongo import *
 from datetime import *
 from time import *
 
-
-
 class baseDatosClass():
     apikey = "AkF2jtWisCYb4wZoibvcPO60HUUQRKC1"
    
     def conexion(self):
-       #conexion= Connection()
        MONGODB_URI = "mongodb://MadAlert:tfg20172018@ds235388.mlab.com:35388/noticias"
        conexion = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
        return conexion.get_default_database()
-       # db = conexion['noticias'] #conexion a la base de datos que utilizamos
        
     def conexionAlertas(self, conexion):
-        #db = conexion.noticias local
-        #coleccion = db.alertas local
-        coleccion = conexion.alertas #coleccion
+        coleccion = conexion.alertas 
         return coleccion
     
     def conexionEstadisticas(self, conexion):
-        #db = conexion.noticias
-        #coleccion = db.estadisticas #coleccion
-        coleccion = conexion.estadisticas #coleccion
+        coleccion = conexion.estadisticas 
         return coleccion
 
     def conexionEstSeguridad(self, conexion):
-        #db = conexion.noticias
-        #coleccion = db.estSeguridad #coleccion
-        coleccion = conexion.estSeguridad #coleccion
+        coleccion = conexion.estSeguridad 
         return coleccion
 
     def conexionEstDetenidos(self, conexion):
-        #db = conexion.noticias
-        #coleccion = db.estDetenidos #coleccion
-        coleccion = conexion.estDetenidos #coleccion
+        coleccion = conexion.estDetenidos 
         return coleccion    
     
     def conexionEstAccidentes(self, conexion):
-        #db = conexion.noticias
-        #coleccion = db.estAccidentes #coleccion
-        coleccion = conexion.estAccidentes #coleccion
+        coleccion = conexion.estAccidentes 
         return coleccion    
 
     def desconexion(self):
@@ -51,14 +37,12 @@ class baseDatosClass():
         
     def insertarAlerta(self, coleccion, alerta, fecha, url, distrito, categoria, fuente):
         diccionario = {"alerta": alerta, "fecha" : fecha, "url" : url, "distrito": distrito, "categoria": categoria, "fuente": fuente}
-        # local -> coleccion.insert(diccionario)
         coleccion.insert_one(diccionario)
 
     def insertarEstadisticas(self, coleccion, distrito, categoria, mes):
         diccionario = {"distrito": distrito, "categoria": categoria, "mes": mes}
         coleccion.insert_one(diccionario)
   
-
     def insertarEstSeguridad(self, coleccion, distrito, personas, patrimonio, armas, ten_drogas, con_drogas, mes):
         diccionario = {"distrito": distrito, "personas": personas, "patrimonio": patrimonio,
                         "armas": armas, "ten_drogas": ten_drogas, "con_drogas": con_drogas, "mes" : mes}
